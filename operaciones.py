19cffd1dec5d79803361c48909f739e30bd42186
jesusMava <jesusmacedo7@hgmail.com>
2017-07-22 12:41:56 -0500

    copiar la carpeta

---
+++ operaciones.py
@@ -0,0 +1,31 @@
+#NOM: Operaciones .py
+#Objeti3vo Muestra las funciones en Python
+#Autor : Alumnos de automatas 2 
+#Fecha 04-jul-2017
+
+
+#Funcion para sumar
+def sum(num1,num2):
+	return num1+num2
+
+#Funcion para restar 2 numeros
+def res (num1,num2):
+	return num1-num2
+
+#Funcion para Multiplicar
+def Mul (num1,num2):
+	return num1*num2
+
+
+
+#Funcion para dividir 
+#
+def Div (num1,num2):
+	return float(num1)/num2
+
+
+#Modulo inicial
+print "La suma es: ",sum(2,3)
+print "La resta es ",res(2,3)
+print "La Multiplicacion es ",Mul(2,3)
+print "La Divicion es ", Div(6,2)
